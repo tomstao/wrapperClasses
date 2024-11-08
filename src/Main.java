3c2854ce1c8a1dacd664e8e3626e3ed74365cf0e
tomstao <96sutao@gmail.com>
2024-11-08 11:54:39 -0500

    wrapper classes

--- src/Main.java
+++ src/Main.java
@@ -16,14 +16,15 @@
 // unboxing = the reverse of autoboxing. Automatic conversion of wrapper class to primitive
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // autoboxing
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Bro";
+        if (a == true){
+            System.out.println("true");
         }
+
     }
 }
